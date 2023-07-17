1e91d0fa9bae1cad4f595580622082bf4493743b
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-07-17 10:44:17 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"Hey {message.from_user.first_name}\n\nWelcome to {Anony.mention}\n\n๏ To Save To Timer Photos And Timer Video\nGenerating Session\n\nBy @Black_Devil_Admin.",
+        text=f"Hey {message.from_user.first_name}\n\nWelcome to {Anony.mention}\n\n๏ To Save To Timer Photos And Timer Video\n𝙲𝚘𝚗𝚗𝚎𝚌𝚝 𝚃𝚘 𝙱𝚘𝚝\n\nBy @Black_Devil_Admin.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
